bf3c70248d78d2d408c0ae3c12d510599236eef3
Jonathon Mullins <jonathonm93@gmail.com>
2016-09-23 11:53:01 -0600

    started game work

---
+++ js/game.js
@@ -0,0 +1,25 @@
+var mainPlayer = {
+    name: "Player",
+    rank: "Pawn",
+    speed: 10,
+    power: 1,
+
+    messenger: function (messege) {
+        alert("Is this correct? " + messege);
+    }
+}
+
+mainPlayer.name = prompt("Enter your name.");
+
+alert("Welcome " + mainPlayer.name);
+
+var phrases = [
+ prompt("Please enter mother's maiden name."),
+ prompt("Enter your phone number."), 
+ prompt("Enter your credit card number."),
+ prompt("Who is your first born child.")
+    ];
+
+for (var i = 0 ; i < phrases.length ; i++) {
+    mainPlayer.messenger(phrases[i]);
+}
